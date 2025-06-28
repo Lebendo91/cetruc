915e85868c22904a1df5a33ecfb0eef3559fd34e
Lebendo91 <ibrahim.boughalem06@gmail.com>
2025-06-28 11:04:19 +0200

    Update server.py

--- server.py
+++ server.py
@@ -84,4 +84,6 @@ def cancel():
     return "<h1>Paiement annulé</h1><p>La transaction a été interrompue.</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
